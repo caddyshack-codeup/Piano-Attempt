47e0976125069d69ddca0572205354ee6137c853
jerlogan <jeremy.logan.web@gmail.com>
2019-03-13 23:12:55 -0500

    basic drum functionality

--- js/piano-js.js
+++ js/piano-js.js
@@ -1,6 +1,13 @@
 (function(){
     "use strict";
 
+
+    const tKey = $('#t-key')[0];
+    const yKey = $('#y-key')[0];
+    const fKey = $('#f-key')[0];
+    const gKey = $('#g-key')[0];
+    const bKey = $('#b-key')[0];
+    const hKey = $('#h-key')[0];
     const jKey = $('#j-key')[0];
 
 
@@ -8,26 +15,32 @@
 
         if (e.which === 84) {
             console.log('t');
+            tKey.play();
         }
 
         else if (e.which === 89) {
             console.log('y');
+            yKey.play();
         }
 
         else if (e.which === 70) {
             console.log('f');
+            fKey.play();
         }
 
         else if (e.which === 71) {
             console.log('g');
+            gKey.play();
         }
 
         else if (e.which === 66) {
             console.log('b');
+            bKey.play();
         }
 
         else if (e.which === 72) {
             console.log('h');
+            hKey.play();
         }
 
         else if (e.which === 74) {
